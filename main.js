040f9ecd6d1461ee61182575edec35218fd153d7
navruzx <nnavruz095@gmail.com>
2024-07-03 17:54:42 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+let a = 5
+let P = 4*a
+let a = 2;
+let S = a**2
+console.log(S)
+let a = 5
+let b = 5
+let S = a*b
+let P = 2(a+b)
+let d = 6
+let p = 3.14
+let l = p*d 
+console.log(l);
+let a = 5
+let v = a**3 
+let s = 6*a**2
+console.log(s);
+let a = 5 
+b = 5 
+c = 5 ;
+let v = a*b*c
+let s=2*(a*b+b*c+a*c)
+console.log(s);
+let r = 5 
+let l = 2*p*r 
+let s = p*r**2
+console.log(s);
+console.log(l);
+let a = 5
+let b = 5
+let p = (a+b)/2 
+console.log(p);
+let a = 5
+let b = 5
+let p = a*b
+console.log(p);
+
